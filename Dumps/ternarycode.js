4909af7978280e4a09e5a63f5b5c7cd8bb55ce2b
RupeshKotal <127592131+RupeshKotal@users.noreply.github.com>
2023-08-11 22:40:11 +0530

    Add files via upload

---
+++ Dumps/ternarycode.js
@@ -0,0 +1,14 @@
+let userLoggedIn = false
+
+userLoggedIn ? console.log("welcome to the website") : console.log("please loged in")
+
+/*
+Note :- 
+
+syntax : condition ? condition 1 : condition 2
+
+in this (;) dont give this symbole
+? after this it checks (true) condition
+: after this it checkes (false condition)  
+we can use this as if else but for small operation but it is very confusing use if else loop
+*/
