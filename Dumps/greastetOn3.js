c99309486eaf2d02fe4148bc0b03f6fd1ebf361b
RupeshKotal <127592131+RupeshKotal@users.noreply.github.com>
2023-08-11 22:39:50 +0530

    Add files via upload

---
+++ Dumps/greastetOn3.js
@@ -0,0 +1,17 @@
+let a= 84;
+let b= 74;
+let c=54;
+
+if (a > b){
+    if(a >c){
+        console.log("A is greatest among all")
+    }
+}
+else{
+    if(b > c){
+        console.log("B is gretest among all")
+    }
+    else{
+        console.log("C is geratest ammong all")
+    }
+}
