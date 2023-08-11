3aa24ba56347c57bf651705df5830f4e7acc5f92
RupeshKotal <127592131+RupeshKotal@users.noreply.github.com>
2023-08-11 22:40:31 +0530

    Add files via upload

---
+++ Dumps/while.js
@@ -0,0 +1,27 @@
+// Syntax 
+
+/*
+while (condition) {
+    
+}
+*/
+
+/*
+let r =1;
+let end = 10;
+
+while (r <= 10) {
+    console.log(r);
+    r++
+}
+*/
+
+// Do while 
+
+let r= 12;
+let end =10;
+
+do{
+    console.log(r);
+    r++
+}while(r <= end)
