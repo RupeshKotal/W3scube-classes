daeb7181916ae83e89f90c68638f4ccd3d7abd2c
RupeshKotal <127592131+RupeshKotal@users.noreply.github.com>
2023-08-11 22:39:32 +0530

    Add files via upload

---
+++ Dumps/forloop.js
@@ -0,0 +1,36 @@
+// Synatx 
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+/*
+
+for(let i=0; i <=10; i++){
+
+    for(let r= 10; r <= 14; r++){
+        console.log(r)
+    }
+
+    console.log(i);
+}
+
+*/
+
+// Break and continue 
+
+/*
+for(let i=0; i <=10; i++){
+
+    if(i === 4){
+         //break
+
+         console.log("this is 4");
+         continue
+    }
+
+    console.log(i);
+}
+*/
+
